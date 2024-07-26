c71cd360765e873d2174af4eff902f5c9aef58e7
Luciano Epifani <lfrancoepifani@gmail.com>
2024-07-26 19:51:06 +0200

    Update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// index.js
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,
@@ -10,7 +10,6 @@ const connection = mysql.createConnection({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB
 });
-
 
 connection.connect(err => {
   if (err) {
@@ -20,14 +19,17 @@ connection.connect(err => {
   console.log('Connected to MySQL');
 });
 
-app.get('/', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (error) {
-    res.status(500).send('Error al obtener los usuarios');
-  }
+app.get('/', (req, res) => {
+  connection.query('SELECT * FROM users', (err, results) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Error al obtener los usuarios: ' + err.message);
+      return;
+    }
+    res.json(results);
+  });
 });
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
